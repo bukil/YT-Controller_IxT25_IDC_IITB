ae5be58b13b0dc0f1ccbee9e134f720fe97b8690
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-02-21 23:02:01 +0530

    qmk based files

---
+++ rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+BOOTLOADER = rp2040
+ENCODER_ENABLE = yes		#enable rotary encoders
+BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keyseeeebbbbbeeeeeebbbbbebe
+EXTRAKEY_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+LTO_ENABLE = yes
+VIA_ENABLE = yes
+TAPDANCE_ENABLE = yes
